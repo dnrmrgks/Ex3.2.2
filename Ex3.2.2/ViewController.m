0ae2f0beaaa6df76cb892bb79ce37a33e518320d
park sung chan <dnrmrgks@naver.com>
2014-01-06 13:53:37 +0900

    3.2.2
    3.2.2

--- Ex3.2.2/ViewController.m
+++ Ex3.2.2/ViewController.m
@@ -8,12 +8,51 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UIActionSheetDelegate>
 
 @end
 
 @implementation ViewController
+{
+    
+    UIActionSheet *sheet2;
+    int count;
+}
 
+-(void)actionSheet:(UIActionSheet *)actionSheet clickedButtonAtIndex:(NSInteger)buttonIndex{
+    NSLog(@"selected index : %d" ,buttonIndex);
+    
+}
+
+-(void)willPresentActionSheet:(UIActionSheet *)actionSheet{
+    if(2 == actionSheet.tag){
+        NSString *title = [NSString stringWithFormat:@"%d 번쨰" , count++];
+        actionSheet.title = title;
+        
+    }
+}
+
+
+-(IBAction)showBasicActionSheet:(id)sender{
+    UIActionSheet *sheet = [[UIActionSheet alloc]initWithTitle:@"제목" delegate:self cancelButtonTitle:@"취소" destructiveButtonTitle:@"중요" otherButtonTitles:@"확인1",@"확인2", nil];
+    
+    //sheet .retainCount 1
+    sheet.tag = 1;
+    [sheet showInView:self.view];
+    
+}
+
+
+-(IBAction)showActionSheetWithCount :(id)sender{
+    
+    //게으른 초기화 lazy initialization
+    if(nil == sheet2){
+        sheet2 =[[UIActionSheet alloc]initWithTitle:@"제목" delegate:self cancelButtonTitle:nil destructiveButtonTitle:nil otherButtonTitles:@"선택1",@"선택2", nil];
+        
+        sheet2.tag = 2;
+    }
+    [sheet2 showInView:self.view];
+}
 - (void)viewDidLoad
 {
     [super viewDidLoad];
